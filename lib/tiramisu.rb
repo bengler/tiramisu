6a7c9cbabcff6c4bc03c38728b0300bf078f0094
Katrina Owen <katrina.owen@gmail.com>
2012-11-14 10:33:46 +0100

    Refactor config

--- lib/tiramisu.rb
+++ lib/tiramisu.rb
@@ -1,18 +1,34 @@
-Dir.glob('./lib/**/*.rb').each { |lib| require lib }
+Dir.glob('./lib/tiramisu/**/*.rb').each { |lib| require lib }
 
 module Tiramisu
   def self.config
-    if ENV['RACK_ENV'] == 'test'
-      @config ||= {
-        "S3" => {
-          "access_key_id" => ENV.fetch('TIRAMISU_S3_KEY'),
-          "secret_access_key" => ENV.fetch('TIRAMISU_S3_SECRET_KEY'),
-          "bucket" => ENV.fetch('TIRAMISU_S3_BUCKET')
-        },
-        "tootsie"=>"http://localhost:9000"
-      }
-    else
-      @config ||= YAML::load(File.open("config/services.yml"))[ENV['RACK_ENV']]
-    end
+    @config ||= test_config if test?
+    @config ||= service_config
+  end
+
+  def self.environment
+    ENV['RACK_ENV'] ||= 'development'
+  end
+
+  def self.test?
+    environment == 'test'
+  end
+
+  private
+
+  def self.test_config
+    {
+      "S3" => {
+        "access_key_id" => ENV.fetch('TIRAMISU_S3_KEY'),
+        "secret_access_key" => ENV.fetch('TIRAMISU_S3_SECRET_KEY'),
+        "bucket" => ENV.fetch('TIRAMISU_S3_BUCKET')
+      },
+      "tootsie"=>"http://localhost:9000"
+    }
+  end
+
+  def self.service_config
+    YAML::load(File.open("config/services.yml"))[environment]
   end
 end
+
