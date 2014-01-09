15931e9b4505abbdd46342dc0ed48283d3565de4
Bjørge Næss <bjoerge@bengler.no>
2014-01-09 15:47:17 +0100

    Set correct aspect ratio for images with EXIF:Orientation

--- spec/api/v1/images_spec.rb
+++ spec/api/v1/images_spec.rb
@@ -11,8 +11,11 @@ describe 'API v1' do
   describe 'POST /images/:uid' do
 
     let(:chunked_json_response) { last_response.body.split("\n").map { |chunk| JSON.parse(chunk) } }
-    let(:image_from_fixture) {
-      {:file => 'spec/fixtures/ullevaalseter.jpg', :aspect_ratio => 1.499}
+    let(:uploaded_image) {
+      'spec/fixtures/ullevaalseter.jpg'
+    }
+    let(:rotated_image) {
+      'spec/fixtures/rotated.jpg'
     }
 
     it "submits an image and returns a chunked json response with progress data and finally the image hash" do
@@ -24,7 +27,7 @@ describe 'API v1' do
       Pebblebed::GenericClient.any_instance.should_receive(:post).with("/jobs", anything()).once
 
       VCR.use_cassette('S3', :match_requests_on => [:method, :host]) do
-        post "/images/image:realm.app.collection.box$", :file => Rack::Test::UploadedFile.new(image_from_fixture[:file], "image/jpeg")
+        post "/images/image:realm.app.collection.box$", :file => Rack::Test::UploadedFile.new(uploaded_image, "image/jpeg")
       end
 
       last_response.status.should eq(200)
@@ -44,19 +47,44 @@ describe 'API v1' do
       oid.should_not be_nil
 
       timestamp, aspect_ratio, rand= oid.split("-")
-      aspect_ratio.to_i.should eq(image_from_fixture[:aspect_ratio]*1000)
+      aspect_ratio.to_i.should eq 1499
 
       image['baseurl'].should match(/http\:\/\/.+\/#{path.split(".").join("/")}\/#{timestamp}-#{aspect_ratio}-#{rand}/)
       image['versions'].map { |s| s['width'] }.should eq([100, 100, 300, 500, 700, 1000, 5000])
 
       image['original'].should match(/#{image['baseurl']}\/original.jpg/)
-      image['aspect_ratio'].to_f.should be_within(0.01).of(1.49)
+      image['aspect_ratio'].to_f.should be_within(0.001).of(1.499)
+    end
+
+    it "calculates correct aspect ratio for rotated images" do
+
+      AssetStore.any_instance.should_receive(:put).once do |url, intercepted|
+        while intercepted.read(intercepted.size.to_f / 5.0) ; end # causes progress to be reported
+      end
+
+      Pebblebed::GenericClient.any_instance.should_receive(:post).with("/jobs", anything()).once
+
+      VCR.use_cassette('S3', :match_requests_on => [:method, :host]) do
+        post "/images/image:realm.app.collection.box$", :file => Rack::Test::UploadedFile.new(rotated_image, "image/jpeg")
+      end
+
+      last_response.status.should eq(200)
+      chunks = chunked_json_response
+
+      image = chunks.last['metadata']
+      image.should_not be_nil
+      oid = Pebbles::Uid.parse(image['uid']).last
+      oid.should_not be_nil
+
+      _, aspect_ratio, _ = oid.split("-")
+
+      aspect_ratio.to_i.should eq 558
+
+      image['aspect_ratio'].to_f.should be_within(0.001).of(0.558)
     end
 
     it "returns failure as last json chunk if uploaded file are of wrong format" do
 
-      AssetStore.any_instance.should_not_receive(:put)
-      
       Pebblebed::Connector.any_instance.should_not_receive(:post)
 
       post "/images/image:realm.app.collection.box$*", :file => Rack::Test::UploadedFile.new('spec/fixtures/unsupported-format.xml')
@@ -67,13 +95,13 @@ describe 'API v1' do
       chunks.last['message'].should eq('format-not-supported')
       chunks.last['percent'].should eq(100)
     end
-
+    
     it "returns failure as last json chunk and includes the error message if something unexpected happens" do
       AssetStore.any_instance.should_receive(:put).once.and_raise("Unexpected error") # just to make something fail
 
       Pebblebed::Connector.any_instance.should_not_receive(:post)
 
-      post "/images/image:realm.app.collection.box$", :file => Rack::Test::UploadedFile.new(image_from_fixture[:file], "image/jpeg")
+      post "/images/image:realm.app.collection.box$", :file => Rack::Test::UploadedFile.new(uploaded_image, "image/jpeg")
 
       last_response.status.should eq(200)
       chunks = chunked_json_response
