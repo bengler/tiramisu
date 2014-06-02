a381914bfafd7da1fd74028c7511d3c59f440a5d
Bjørge Næss <bjoerge@gmail.com>
2014-06-02 14:04:59 +0200

    Give config file precedence if it exists (avoids having to set env variables for s3 credentials)

--- lib/tiramisu/config.rb
+++ lib/tiramisu/config.rb
@@ -1,29 +1,32 @@
 module Tiramisu
   class Config
 
+    CONFIG_FILE_PATH = "config/services.yml"
+  
     attr_reader :environment
+
     def initialize(environment)
       @environment = environment
     end
 
     def settings
-      @settings ||= test? ? test_config : config
-    end
-
-    def test?
-      environment == 'test'
+      @settings ||= config_exists? ? config : ci_config
     end
 
     private
 
     def config
-      YAML::load(File.open("config/services.yml"))[environment]
+      YAML::load(File.open(CONFIG_FILE_PATH))[environment]
+    end
+
+    def config_exists?
+      File.exists?(CONFIG_FILE_PATH)
     end
 
     # Allows secrets to be stored in environment so that
     # they do not need to be committed to local repositories.
     # This makes it possible to run on the CI server.
-    def test_config
+    def ci_config
       {
         "S3" => {
           "access_key_id" => ENV.fetch('TIRAMISU_S3_KEY'),
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ require 'config/environment'
 
 require 'rack/test'
 
+require 'lib/tiramisu'
 require 'api/v1'
 
 require 'vcr'
@@ -18,9 +19,9 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
-  c.filter_sensitive_data("<TIRAMISU S3 KEY>") { ENV.fetch('TIRAMISU_S3_KEY') }
-  c.filter_sensitive_data("<TIRAMISU S3 SECRET KEY>") { ENV.fetch('TIRAMISU_S3_SECRET_KEY') }
-  c.filter_sensitive_data("<TIRAMISU S3 BUCKET>") { ENV.fetch('TIRAMISU_S3_BUCKET') }
+  c.filter_sensitive_data("<TIRAMISU S3 KEY>") { Tiramisu.config['S3']['access_key_id'] }
+  c.filter_sensitive_data("<TIRAMISU S3 SECRET KEY>") { Tiramisu.config['S3']['secret_access_key'] }
+  c.filter_sensitive_data("<TIRAMISU S3 BUCKET>") { Tiramisu.config['S3']['bucket'] }
 end
 
 set :environment, :test
